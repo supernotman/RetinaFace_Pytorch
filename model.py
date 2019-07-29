a35ee585563b16d8febda5822f388dee5a6b0223
yangziyang <superalvin123@foxmail.com>
2019-07-29 16:53:13 +0800

    modify

--- model.py
+++ model.py
@@ -333,26 +333,6 @@ class ResNet(nn.Module):
 
             return classifications, bboxes, landmarks
 
-        #     scores = torch.max(classification, dim=2, keepdim=True)[0]
-
-        #     scores_over_thresh = (scores>0.05)[0, :, 0]
-
-        #     if scores_over_thresh.sum() == 0:
-        #         # no boxes to NMS, just return
-        #         return [torch.zeros(0), torch.zeros(0), torch.zeros(0, 4)]
-
-        #     classification = classification[:, scores_over_thresh, :]
-        #     transformed_anchors = transformed_anchors[:, scores_over_thresh, :]
-        #     scores = scores[:, scores_over_thresh, :]
-
-        #     anchors_nms_idx = nms(torch.cat([transformed_anchors, scores], dim=2)[0, :, :], 0.5)
-
-        #     nms_scores, nms_class = classification[0, anchors_nms_idx, :].max(dim=1)
-
-        #     return [nms_scores, nms_class, transformed_anchors[0, anchors_nms_idx, :]]
-
-
-
 def resnet18(num_classes, pretrained=False, **kwargs):
     """Constructs a ResNet-18 model.
     Args:
