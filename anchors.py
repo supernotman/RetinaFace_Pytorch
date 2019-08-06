8b8466ca13ebc736efb81f8de2d2f8ea2eff7684
yangziyang <superalvin123@foxmail.com>
2019-08-06 14:10:31 +0800

    change the anchor size

--- anchors.py
+++ anchors.py
@@ -8,22 +8,24 @@ class Anchors(nn.Module):
         super(Anchors, self).__init__()
 
         if pyramid_levels is None:
-            self.pyramid_levels = [2, 3, 4, 5, 6]
+            # self.pyramid_levels = [2, 3, 4, 5, 6]
+            self.pyramid_levels = [3, 4, 5]
         if strides is None:
             self.strides = [2 ** x for x in self.pyramid_levels]
         if sizes is None:
-            self.sizes = [2 ** (x + 2) for x in self.pyramid_levels]
+            # self.sizes = [2 ** (x + 2) for x in self.pyramid_levels]
+            self.sizes = [2 ** 4.0, 2 ** 6.0, 2 ** 8.0]
         if ratios is None:
             self.ratios = np.array([1, 1, 1])
         if scales is None:
-            self.scales = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)])
+            # self.scales = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)])
+            self.scales = np.array([2 ** 0, 2 ** (1/2.0) , 2 ** 1.0 ])
 
     def forward(self, image):
-        
+       
         image_shape = image.shape[2:]
         image_shape = np.array(image_shape)
         image_shapes = [(image_shape + 2 ** x - 1) // (2 ** x) for x in self.pyramid_levels]
-        #print('----image_shape----:',image_shape)
 
         # compute anchors over all pyramid levels
         all_anchors = np.zeros((0, 4)).astype(np.float32)
@@ -77,7 +79,7 @@ def shift(shape, stride, anchors):
     # add A anchors (1, A, 4) to
     # cell K shifts (K, 1, 4) to get
     # shift anchors (K, A, 4)
-    # reshape to (K*A, 4) shifted anchors
+    # reshape to (K * A, 4) shifted anchors
     A = anchors.shape[0]
     K = shifts.shape[0]
     all_anchors = (anchors.reshape((1, A, 4)) + shifts.reshape((1, K, 4)).transpose((1, 0, 2)))
