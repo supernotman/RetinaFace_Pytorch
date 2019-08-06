6a500e952866bb4252234d07a7773ffdc0e7ede9
yangziyang <superalvin123@foxmail.com>
2019-08-06 14:13:52 +0800

    add detect.py

--- detect.py
+++ detect.py
@@ -9,10 +9,11 @@ from PIL import Image
 import cv2
 import torchvision
 import eval_widerface
+import torchvision_model
 import model
 
 def pad_to_square(img, pad_value):
-    c, h, w = img.shape
+    _, h, w = img.shape
     dim_diff = np.abs(h - w)
     # (upper / left) padding and (lower / right) padding
     pad1, pad2 = dim_diff // 2, dim_diff - dim_diff // 2
@@ -23,7 +24,7 @@ def pad_to_square(img, pad_value):
 
     return img, pad
 
-def resize(image, size):
+def resize(image, size)
     image = F.interpolate(image.unsqueeze(0), size=size, mode="nearest").squeeze(0)
     return image
 
@@ -41,18 +42,22 @@ def main():
     args = get_args()
 
 	# Create the model
-    if args.depth == 18:
-        RetinaFace = model.resnet18(num_classes=2, pretrained=True)
-    elif args.depth == 34:
-        RetinaFace = model.resnet34(num_classes=2, pretrained=True)
-    elif args.depth == 50:
-        RetinaFace = model.resnet50(num_classes=2, pretrained=True)
-    elif args.depth == 101:
-        RetinaFace = model.resnet101(num_classes=2, pretrained=True)
-    elif args.depth == 152:
-        RetinaFace = model.resnet152(num_classes=2, pretrained=True)
-    else:
-        raise ValueError('Unsupported model depth, must be one of 18, 34, 50, 101, 152')
+    # if args.depth == 18:
+    #     RetinaFace = model.resnet18(num_classes=2, pretrained=True)
+    # elif args.depth == 34:
+    #     RetinaFace = model.resnet34(num_classes=2, pretrained=True)
+    # elif args.depth == 50:
+    #     RetinaFace = model.resnet50(num_classes=2, pretrained=True)
+    # elif args.depth == 101:
+    #     RetinaFace = model.resnet101(num_classes=2, pretrained=True)
+    # elif args.depth == 152:
+    #     RetinaFace = model.resnet152(num_classes=2, pretrained=True)
+    # else:
+    #     raise ValueError('Unsupported model depth, must be one of 18, 34, 50, 101, 152')
+
+    # Create torchvision model
+    return_layers = {'layer2':1,'layer3':2,'layer4':3}
+    RetinaFace = torchvision_model.create_retinaface(return_layers)
 
     # Load trained model
     retina_dict = RetinaFace.state_dict()
@@ -61,33 +66,34 @@ def main():
     RetinaFace.load_state_dict(pretrained_dict)
 
     RetinaFace = RetinaFace.cuda()
+    RetinaFace.eval()
 
     # Read image
     img = skimage.io.imread(args.image_path)
     img = torch.from_numpy(img)
     img = img.permute(2,0,1)
     padded_img, _ = pad_to_square(img,0)
-    #print('padded img:',padded_img)
     resized_img = resize(padded_img.float(),(640,640))
     input_img = resized_img.unsqueeze(0).cuda()
-    picked_boxes, picked_landmarks = eval_widerface.get_detections(input_img, RetinaFace, score_threshold=0.05, iou_threshold=0.3)
+    picked_boxes, picked_landmarks = eval_widerface.get_detections(input_img, RetinaFace, score_threshold=0.5, iou_threshold=0.3)
 
-    #np_img = resized_img.cpu().numpy()
     np_img = resized_img.cpu().permute(1,2,0).numpy()
     np_img.astype(int)
     img = cv2.cvtColor(np_img.astype(np.uint8),cv2.COLOR_BGR2RGB)
 
     for j, boxes in enumerate(picked_boxes):
         if boxes is not None:
-            for box in boxes:
+            for box,landmark in zip(boxes,picked_landmarks[j]):
                 cv2.rectangle(img,(box[0],box[1]),(box[2],box[3]),(0,0,255),thickness=2)
+                cv2.circle(img,(landmark[0],landmark[1]),radius=1,color=(0,0,255),thickness=2)
+                cv2.circle(img,(landmark[2],landmark[3]),radius=1,color=(0,255,0),thickness=2)
+                cv2.circle(img,(landmark[4],landmark[5]),radius=1,color=(255,0,0),thickness=2)
+                cv2.circle(img,(landmark[6],landmark[7]),radius=1,color=(0,255,255),thickness=2)
+                cv2.circle(img,(landmark[8],landmark[9]),radius=1,color=(255,255,0),thickness=2)
 
-    cv2.imshow('test',img)
+
+    cv2.imshow('RetinaFace-Pytorch',img)
     cv2.waitKey()
-
-
-
-
 
 if __name__=='__main__':
     main()
