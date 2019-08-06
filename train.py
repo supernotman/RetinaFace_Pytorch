c7ac148e4e7669eab395ab4a1e44b2948eda1ba3
yangziyang <superalvin123@foxmail.com>
2019-08-06 14:18:34 +0800

    change optimizer from adam to sgd and add losses log

--- train.py
+++ train.py
@@ -4,15 +4,18 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, models, transforms
-from dataloader import TrainDataset, ValDataset, collater, RandomCroper, RandomFlip
+from dataloader import TrainDataset, ValDataset, collater, RandomCroper, RandomFlip, Resizer, PadToSquare
 from torch.utils.data import Dataset, DataLoader
 from terminaltables import AsciiTable, DoubleTable, SingleTable
+from tensorboardX import SummaryWriter
 from torch.optim import lr_scheduler
 import torch.distributed as dist
 import eval_widerface
 import torchvision
 import model
 import os
+from torch.utils.data.distributed import DistributedSampler
+import torchvision_model
 
 
 def get_args():
@@ -35,47 +38,64 @@ def get_args():
 
 def main():
     args = get_args()
-
     if not os.path.exists(args.save_path):
         os.mkdir(args.save_path)
+    log_path = os.path.join(args.save_path,'log')
+    if not os.path.exists(log_path):
+        os.mkdir(log_path)
+
+    writer = SummaryWriter(log_dir=log_path)
 
     data_path = args.data_path
     train_path = os.path.join(data_path,'train/label.txt')
     val_path = os.path.join(data_path,'val/label.txt')
-    dataset_train = TrainDataset(train_path,transform=transforms.Compose([RandomCroper()]))
-    dataloader_train = DataLoader(dataset_train, num_workers=4, batch_size=args.batch, collate_fn=collater,shuffle=True )
-    dataset_val = ValDataset(val_path,transform=transforms.Compose([RandomCroper()]))
-    dataloader_val = DataLoader(dataset_val, num_workers=4, batch_size=16, collate_fn=collater)
+    # dataset_train = TrainDataset(train_path,transform=transforms.Compose([RandomCroper(),RandomFlip()]))
+    dataset_train = TrainDataset(train_path,transform=transforms.Compose([Resizer(),PadToSquare()]))
+    dataloader_train = DataLoader(dataset_train, num_workers=8, batch_size=args.batch, collate_fn=collater,shuffle=True)
+    # dataset_val = ValDataset(val_path,transform=transforms.Compose([RandomCroper()]))
+    dataset_val = ValDataset(val_path,transform=transforms.Compose([Resizer(),PadToSquare()]))
+    dataloader_val = DataLoader(dataset_val, num_workers=8, batch_size=args.batch, collate_fn=collater)
     
     total_batch = len(dataloader_train)
 
 	# Create the model
-    if args.depth == 18:
-        retinaface = model.resnet18(num_classes=2, pretrained=True)
-    elif args.depth == 34:
-        retinaface = model.resnet34(num_classes=2, pretrained=True)
-    elif args.depth == 50:
-        retinaface = model.resnet50(num_classes=2, pretrained=True)
-    elif args.depth == 101:
-        retinaface = model.resnet101(num_classes=2, pretrained=True)
-    elif args.depth == 152:
-        retinaface = model.resnet152(num_classes=2, pretrained=True)
-    else:
-        raise ValueError('Unsupported model depth, must be one of 18, 34, 50, 101, 152')
+    # if args.depth == 18:
+    #     retinaface = model.resnet18(num_classes=2, pretrained=True)
+    # elif args.depth == 34:
+    #     retinaface = model.resnet34(num_classes=2, pretrained=True)
+    # elif args.depth == 50:
+    #     retinaface = model.resnet50(num_classes=2, pretrained=True)
+    # elif args.depth == 101:
+    #     retinaface = model.resnet101(num_classes=2, pretrained=True)
+    # elif args.depth == 152:
+    #     retinaface = model.resnet152(num_classes=2, pretrained=True)
+    # else:
+    #     raise ValueError('Unsupported model depth, must be one of 18, 34, 50, 101, 152')
+
+    # Create torchvision model
+    return_layers = {'layer2':1,'layer3':2,'layer4':3}
+    retinaface = torchvision_model.create_retinaface(return_layers)
 
     retinaface = retinaface.cuda()
     retinaface = torch.nn.DataParallel(retinaface).cuda()
     retinaface.training = True
 
-    #optimizer = optim.Adam(retinaface.parameters(), lr=1e-5)
-    optimizer = optim.Adam(retinaface.parameters(), lr=1e-3,weight_decay=0.0005)
+    optimizer = optim.Adam(retinaface.parameters(), lr=1e-3)
+    # optimizer = optim.SGD(retinaface.parameters(), lr=1e-2, momentum=0.9, weight_decay=0.0005)
+    # scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
+    # scheduler  = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+    #scheduler  = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10,30,60], gamma=0.1)
 
     print('Start to train.')
 
+    epoch_loss = []
+    iteration = 0
+
     for epoch in range(args.epochs):
         retinaface.train()
+        #print('Current learning rate:',scheduler.get_lr()[0])
         # retinaface.module.freeze_bn()
-        retinaface.module.freeze_first_layer()
+        # retinaface.module.freeze_first_layer()
 
         # Training
         for iter_num,data in enumerate(dataloader_train):
@@ -85,10 +105,12 @@ def main():
             bbox_regression_loss = bbox_regression_loss.mean()
             ldm_regression_loss = ldm_regression_loss.mean()
 
-            loss = classification_loss + 1.0 * bbox_regression_loss + 0.5 * ldm_regression_loss
+            # loss = classification_loss + 1.0 * bbox_regression_loss + 0.5 * ldm_regression_loss
+            loss = classification_loss + bbox_regression_loss + ldm_regression_loss
 
             loss.backward()
             optimizer.step()
+            #epoch_loss.append(loss.item())
             
             if iter_num % args.verbose == 0:
                 log_str = "\n---- [Epoch %d/%d, Batch %d/%d] ----\n" % (epoch, args.epochs, iter_num, total_batch)
@@ -104,6 +126,18 @@ def main():
                 #table = DoubleTable(table_data)
                 log_str +=table.table
                 print(log_str)
+                # write the log to tensorboard
+                writer.add_scalars('losses:',
+                {'total_loss': loss.item(),
+                'cls_loss': classification_loss.item(),
+                'bbox_loss': bbox_regression_loss.item(),
+                'ldm_loss': ldm_regression_loss.item()
+                },
+                iteration * args.verbose)
+                iteration +=1
+        
+        #scheduler.step()
+        #scheduler.step(np.mean(epoch_loss))	
 
         # Eval
         if epoch % args.eval_step == 0:
